b6a9101e554a2682645dbb201a15f84d4dbcab61
anand9589 <anand9589@gmail.com>
2026-02-07 16:12:26 -0500

    1653. Minimum Deletions to Make String Balanced

--- C#/Leetcode/Leetcode/February.cs
+++ C#/Leetcode/Leetcode/February.cs
@@ -2,6 +2,9 @@
 {
     internal class February
     {
+        #region x -->
+        #endregion
+
         #region 1 --> 3010. Divide an Array Into Subarrays With Minimum Cost I
         public int MinimumCost(int[] nums)
         {
@@ -33,6 +36,46 @@
             }
 
             return min + min1 + min2;
+        }
+        #endregion
+
+        #region 7 --> 1653. Minimum Deletions to Make String Balanced
+        public int MinimumDeletions(string s)
+        {
+            int b = 0, d = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'b')
+                {
+                    b++;
+                }
+                else if(b>0)
+                {
+                    d++;
+                    b--;
+                }
+            }
+
+            return d;
+        }
+        public int MinimumDeletions1(string s)
+        {
+            int[] dp = new int[s.Length + 1];
+            int b = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'b')
+                {
+                    b++;
+                    dp[i + 1] = dp[i];
+                }
+                else
+                {
+                    dp[i + 1] = Math.Min(dp[i] + 1, b);
+                }
+            }
+
+            return dp[s.Length];
         }
         #endregion
     }
